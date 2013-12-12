1204155cf2e2234a10724530fdea3ead07a4f42d
Irfan Mahfudz Guntur <ayes@bsmsite.com>
2013-12-12 12:37:15 +0800

    Initial commit

--- application/views/admin/login_view.php
+++ application/views/admin/login_view.php
@@ -30,7 +30,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="/">Faza Shop</a>
+          <a class="navbar-brand" href="/"><?php echo $this->tools_model->getShopName(); ?></a>
         </div>
         <div class="collapse navbar-collapse">
           
