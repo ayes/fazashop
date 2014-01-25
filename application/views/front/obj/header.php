f23e075eb6f46640e05938ff3fd5ce82897f9524
Irfan Mahfudz Guntur <ayes@bsmsite.com>
2014-01-25 22:34:34 +0800

    Initial commit

--- application/views/front/obj/header.php
+++ application/views/front/obj/header.php
@@ -9,7 +9,33 @@
         <?php echo link_tag('http://tools.bsmsite.com/template/fazashop/icon/favicon.ico', 'shortcut icon', 'image/x-icon'); ?>
         <title><?php echo $this->tools_model->getShopName(); ?></title>
         <!-- Bootstrap core CSS -->
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,200,300,600,900,700" rel="stylesheet" type="text/css" />
         <link href="http://tools.bsmsite.com/template/bootstrap/css/bootstrap.css" rel="stylesheet">
+        <style>
+            body {
+  font-family: 'Source Sans Pro', sans-serif;
+  font-size: 15px;
+  line-height: 1.428571429;
+  color: #333333;
+  background-color: #ffffff;
+}h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+.h1,
+.h2,
+.h3,
+.h4,
+.h5,
+.h6 {
+  font-family: 'Source Sans Pro', sans-serif;
+  font-weight: 500;
+  line-height: 1.1;
+  color: inherit;
+}
+        </style>
         <!-- Custom styles for this template -->
         <link href="http://tools.bsmsite.com/template/fazashop/css/fazashop.css" rel="stylesheet">
         <link href="http://tools.bsmsite.com/template/global/css/carousel.css" rel="stylesheet">
